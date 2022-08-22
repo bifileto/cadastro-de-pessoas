12a573587b722b1ab12833fb8be43dffc5a748ec
Bianca <bifileto@gmail.com>
2022-08-21 22:57:13 -0300

    feito o array para salvar os objetos chave e valor de nome e idade

--- Cadastro.js
+++ Cadastro.js
@@ -1,12 +1,26 @@
 let nome;
 let idade;
+let ArrayCadastro = [];
 
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function inserirNoArray(nome, idade){
+  console.log("xxc");
+  ArrayCadastro.push({nome: nome, idade: idade});
+  
+}
 function validaFormulario(){
     if(document.getElementById("nome").value != "" && document.getElementById("idade").value != ""){
     nome = document.getElementById("nome").value;
     idade = document.getElementById("idade").value;
+
+    inserirNoArray(nome, idade);
+    console.log(ArrayCadastro);
+    console.log("cu");
+
   }else{
     alert("Por favor, preencha os campos nome e idade")
   }
 }
+
+
