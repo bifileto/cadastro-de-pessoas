8dcfbfec17315832e7f6696a607468dc4fb21620
Bianca <bifileto@gmail.com>
2022-08-23 15:12:37 -0300

    feitos novos arrays ordenados por idade e ordem alfabetica

--- Cadastro.js
+++ Cadastro.js
@@ -5,7 +5,6 @@ let ArrayCadastro = [];
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
 function inserirNoArray(nome, idade){
-  console.log("xxc");
   ArrayCadastro.push({nome: nome, idade: idade});
   
 }
@@ -15,12 +14,26 @@ function validaFormulario(){
     idade = document.getElementById("idade").value;
 
     inserirNoArray(nome, idade);
-    console.log(ArrayCadastro);
-    console.log("cu");
+    console.log("Array Cadastro" , ArrayCadastro);
+    
+    const ArrayIdade = ArrayCadastro.sort(ordenaIdade);
+    console.log("Array Idade" , ArrayIdade);
+
+    const ArrayNome = ArrayCadastro.sort(ordenaNome);
+    console.log("Array Nome" , ArrayNome);
 
   }else{
     alert("Por favor, preencha os campos nome e idade")
   }
 }
+
+function ordenaIdade(a,b){
+  return a.idade - b.idade;
+}
+
+function ordenaNome(a,b){
+  return a.nome.localeCompare(b.nome);
+}
+
 
 
