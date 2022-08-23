5952dfe8b9803ee7a1e29062b97ed959f5f0a7fc
Bianca <bifileto@gmail.com>
2022-08-23 20:19:07 -0300

    iniciado filtro para classificacao em ordem alfabetica e idade

--- Cadastro.js
+++ Cadastro.js
@@ -2,38 +2,40 @@ let nome;
 let idade;
 let ArrayCadastro = [];
 
-document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario);
 
 function inserirNoArray(nome, idade){
   ArrayCadastro.push({nome: nome, idade: idade});   
 }
 
 function validaFormulario(){
-    if(document.getElementById("nome").value != "" && document.getElementById("idade").value != ""){
+  const naoVazio = document.getElementById("nome").value != "" && document.getElementById("idade").value != "";
+  const idadePositiva = parseInt(document.getElementById("idade").value) >= 0;
+
+    if(naoVazio && idadePositiva){
     nome = document.getElementById("nome").value;
-    idade = document.getElementById("idade").value;
+    idade = parseInt(document.getElementById("idade").value);
 
     inserirNoArray(nome, idade);
-    console.log("Array Cadastro" , ArrayCadastro);
     
     let UltimaPessoaInserida = ArrayCadastro[ArrayCadastro.length - 1];
 
-    console.log(UltimaPessoaInserida);
-
     criarTabela(UltimaPessoaInserida);
-
   }else{
     alert("Por favor, preencha os campos nome e idade")
   }
 }
 
-/*const ArrayIdade = ArrayCadastro.sort(ordenaIdade);
-    console.log("Array Idade" , ArrayIdade);
+document.getElementById("idade").addEventListener("click", ordenaIdade)
+ 
+const ArrayIdade = ArrayCadastro.sort(ordenaIdade);
+console.log("Array Idade" , ArrayIdade);
 
-    const ArrayNome = ArrayCadastro.sort(ordenaNome);
-    console.log("Array Nome" , ArrayNome);*/
+const ArrayNome = ArrayCadastro.sort(ordenaNome);
+console.log("Array Nome" , ArrayNome);
 
 function ordenaIdade(a,b){
+  console.log("Array cadastro fora:", ArrayCadastro)
   return a.idade - b.idade;
 }
 
@@ -42,7 +44,7 @@ function ordenaNome(a,b){
 }
 
 function criarTabela(pessoa){
-  const tabela = document.getElementById("table");
+  const tabela = document.getElementById("tableBody");
     let novaLinha = document.createElement("tr");
     Object.values(pessoa).forEach((valor) => {
       let celula = document.createElement("td");
