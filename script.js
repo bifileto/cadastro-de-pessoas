4d01a659519a6574e5e79ade1cdfc91ae6739450
Bianca <bifileto@gmail.com>
2022-08-24 22:12:18 -0300

    feito refatoração no script.js

--- script.js
+++ script.js
@@ -1,50 +1,21 @@
-let ArrayCadastro = [];
+let pessoas = [];
 const caracteresAceitos = /\w/;
 const botaoEnviar = document.getElementById("botaoEnviar");
 const filtroIdade = document.getElementById("filtroIdade");
 const filtroNome = document.getElementById("filtroNome");
 
-function validaFormulario(nome,idade){
-  const vazio = nome === "" && idade === "";
-  const idadePositiva = parseInt(idade) >= 0;
-  const idadePossivel = parseInt(idade) <= 130;
-  const nomePossivel = caracteresAceitos.test(nome);
-  if(vazio || !idadePositiva || !idadePossivel || !nomePossivel){ 
-    return false;   
-  } else {
-    return true;
-  }
-}
-
-function inserirNoArray(){
+function adicionarPessoa(){
   let nome = document.getElementById("nome").value;
   let idade = document.getElementById("idade").value;
   const faixaEtaria = classificarFaixaEtaria(idade);
-  if(validaFormulario(nome, idade)){
-    ArrayCadastro.push({nome: nome, idade: idade, faixaEtaria: faixaEtaria}); 
-    let UltimaPessoaInserida = ArrayCadastro[ArrayCadastro.length - 1];
-    criarTabela(UltimaPessoaInserida); 
+  if(validarFormulario(nome, idade)){
+    pessoas.push({nome: nome, idade: idade, faixaEtaria: faixaEtaria}); 
+    let ultimaPessoaInserida = pessoas[pessoas.length - 1];
+    criarTabela(ultimaPessoaInserida); 
   } else {
     alert("Por favor, preencha os campos nome e idade com dados válidos");
   }
-}
-
-function filtrarPorIdadeCrescente(){
-ArrayCadastro.sort(ordenaIdade);
-recriarTabela(ArrayCadastro);
-}
-
-function filtrarPorOrdemAlfabetica(){
-ArrayCadastro.sort(ordenaNome);
-recriarTabela(ArrayCadastro);
-}
-
-function ordenaIdade(a,b){
-  return a.idade - b.idade;
-}
-
-function ordenaNome(a,b){
-  return a.nome.localeCompare(b.nome);
+  limparFormulario();
 }
 
 function classificarFaixaEtaria(idade){
@@ -62,6 +33,17 @@ function classificarFaixaEtaria(idade){
   }
 }
 
+function validarFormulario(nome,idade){
+  const vazio = nome === "" && idade === "";
+  const idadeLimite = parseInt(idade) >= 0 && parseInt(idade) <= 130;
+  const nomeAceito = caracteresAceitos.test(nome);
+  if(vazio || !idadeLimite || !nomeAceito){ 
+    return false;   
+  } else {
+    return true;
+  }
+}
+
 function criarTabela(pessoa){
   const tabela = document.getElementById("tableBody");
     let novaLinha = document.createElement("tr");
@@ -73,10 +55,26 @@ function criarTabela(pessoa){
     tabela.appendChild(novaLinha);
 }
 
-function deletarLinhas(tabela){
-  while(tabela.rows.length > 0){
-    tabela.deleteRow(0);
-  }
+function limparFormulario(){
+  document.getElementById("formulario").reset();
+}
+
+function filtrarPorIdadeCrescente(){
+pessoas.sort(ordenarPorIdade);
+recriarTabela(pessoas);
+}
+
+function ordenarPorIdade(a,b){
+  return a.idade - b.idade;
+}
+
+function filtrarPorOrdemAlfabetica(){
+pessoas.sort(ordenarPorNome);
+recriarTabela(pessoas);
+}
+
+function ordenarPorNome(a,b){
+  return a.nome.localeCompare(b.nome);
 }
 
 function recriarTabela(pessoas){
@@ -93,6 +91,12 @@ function recriarTabela(pessoas){
   });
 }
 
-botaoEnviar.addEventListener("click", inserirNoArray);
+function deletarLinhas(tabela){
+  while(tabela.rows.length > 0){
+    tabela.deleteRow(0);
+  }
+}
+
+botaoEnviar.addEventListener("click", adicionarPessoa);
 filtroIdade.addEventListener("click", filtrarPorIdadeCrescente);
 filtroNome.addEventListener("click", filtrarPorOrdemAlfabetica);
